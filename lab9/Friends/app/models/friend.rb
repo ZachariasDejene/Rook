d8adf6606ac52bbaf746004dfbc990f56e37607a
Zacharias Dejene <zmd@andrew.cmu.edu>
2017-03-22 21:15:17 -0400

    mailer

--- lab9/Friends/app/models/friend.rb
+++ lab9/Friends/app/models/friend.rb
@@ -1,2 +1,10 @@
 class Friend < ApplicationRecord
+
+    #Validations
+    validates_presence_of :full_name, :email, :friendship_level
+    validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only"
+    validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, message: "is not a valid format"
+    validates_inclusion_of :friendship_level, in: [1,2,3,4], message: "is not an option", allow_blank: true
+    FRIENDSHIP_LEVELS = [ ["Casual", 1], ["Good", 2], ["Close", 3], ["Best", 4] ]
+
 end
